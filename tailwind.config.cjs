bfcf443b8c769454e9a57f8342aa8860ea8b3415
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-09-01 11:55:01 +0000

    Fix missing Inter font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
@@ -7,6 +9,9 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
