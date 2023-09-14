72dc3385e534fb97270e1408130481f581b6a383
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-09-14 22:45:50 +0000

    Refactor Tailwind config to mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter Variable", ...fontFamily.sans],
+      },
+    },
+  },
+  daisyui: {
+    themes: ["dark"],
+  },
+  plugins: [daisyui],
+};
