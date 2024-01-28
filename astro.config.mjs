9439ba8757ac8cc78e3f669f3cff52591f04b8ae
Shikanime Deva <william.phetsinorath@shikanime.studio>
2024-01-28 22:24:08 +0100

    Add Thread shortlink

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,6 +40,10 @@ export default defineConfig({
       status: 307,
       destination: '/?utm_source=profile&utm_medium=sharing&utm_campaign=myanimelist'
     },
+    '/th': {
+      status: 307,
+      destination: '/?utm_source=profile&utm_medium=sharing&utm_campaign=thread'
+    },
   },
   site: "https://links.shikanime.studio",
 });
