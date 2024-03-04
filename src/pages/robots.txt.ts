a53d21e6e1388bf66d5dfdbb94d931bb3de14358
Shikanime Deva <william.phetsinorath@shikanime.studio>
2024-03-04 19:58:26 +0100

    Migrate to Cloudflare (#11)

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,17 +1,13 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = () => {
-    const sitemapUrl = new URL("sitemap-index.xml", import.meta.env.SITE);
-    return new Response(
-    [
-      "User-agent: *",
-      "Allow: /",
-      `Sitemap: ${sitemapUrl.href}`,
-    ].join("\n"),
+  const sitemapUrl = new URL("sitemap-index.xml", import.meta.env.SITE);
+  return new Response(
+    ["User-agent: *", "Allow: /", `Sitemap: ${sitemapUrl.href}`].join("\n"),
     {
       headers: {
         "Content-Type": "text/plain; charset=utf-8",
       },
-    }
+    },
   );
 };
