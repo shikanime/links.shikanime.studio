9814a2a78e4c7b6e82126a6cdb000db16c11bc20
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-09-16 17:32:32 +0000

    Rework color scheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
 import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
@@ -12,7 +13,21 @@ module.exports = {
     },
   },
   daisyui: {
-    themes: ["dark"],
+    themes: [
+      {
+        void: {
+          primary: "#f3ef00",
+          secondary: "#009af9",
+          accent: "#00977e",
+          neutral: "#000509",
+          "base-100": "#462a7d",
+          info: "#00cbf8",
+          success: "#00a260",
+          warning: "#e59200",
+          error: "#ff4d5e",
+        },
+      },
+    ],
   },
-  plugins: [daisyui],
+  plugins: [typography, daisyui],
 };
